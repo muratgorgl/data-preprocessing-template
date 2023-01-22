2a34c5df04aa38d122755b712fb2c20b5f39154e
Murat Görgülü <105209043+muratgorgl@users.noreply.github.com>
2023-01-22 16:41:21 +0300

    Update VeriOnişlemSablonu.py

--- VeriOnişlemSablonu.py
+++ VeriOnişlemSablonu.py
@@ -5,13 +5,13 @@ Created on Wed Jan 18 13:58:02 2023
 @author: Murat
 """
 
-#1.kütüphaneler
+#1.Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#2.veri önişleme
-#2.1 veri önyükleme
+#2.Data Preprocessing
+#2.1 Data Boot
 veriler= pd.read_csv("veriler.csv")
 #pd.read_csv()
 print(veriler)
@@ -23,7 +23,7 @@ boykilo=veriler[["boy","kilo"]]
 print(boykilo)
 
 
-#eksik veriler
+#Missing Data
 
 #sci-kit learn
 from sklearn.impute import SimpleImputer
@@ -37,7 +37,7 @@ imputer=imputer.fit(Yas[:,1:4]) #fit=öğretmek
 Yas[:,1:4]=imputer.transform(Yas[:,1:4]) #transform= öğrenmek
 print(Yas)
 
-#kategorik verilerden sayısal verilere geçiş (Veri Tipi Dönüştürmesi)
+#Tnsition from Categorical Data to Numerical Data (Data Type Conversion)
 #encoder: Kategorik--> Numeric
 ulke=veriler.iloc[:,0:1].values
 print(ulke)
@@ -54,7 +54,7 @@ ohe=preprocessing.OneHotEncoder()
 ulke=ohe.fit_transform(ulke).toarray()
 print(ulke)
 
-# numpy dizileri DataFrame dönüşümü
+# DataFrame Conversion of Numpy Arrays
 
 sonuc=pd.DataFrame(data=ulke,index=range(22),columns=["fr","tr","us"])
 print(sonuc)
@@ -68,20 +68,20 @@ print(cinsiyet)
 sonuc3=pd.DataFrame(data=cinsiyet,index=range(22),columns=["cinsiyet"])
 print(sonuc3)
 
-#dataframe birleştirme işlemi
+#Dataframe Merge Process
 s=pd.concat([sonuc,sonuc2],axis=1)
 print(s)
 
 s2=pd.concat([s,sonuc3],axis=1)
 print(s2)
 
-#Veri kümesinin eğitim ve test için bölünmesi
+#Division of the Dataset for Training and Testing
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test=train_test_split(s,sonuc3,test_size=0.33,random_state=0)
 
 
-#verilerin ölçeklenmesi
+#Scaling of Data
 from sklearn.preprocessing import StandardScaler
 
 sc=StandardScaler()
